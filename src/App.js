826e1800bcbc187cc727d0f3e1e9f6babe5fbd8c
TTimotti <ttimotti@kakao.com>
2023-11-10 09:41:58 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (evt) => setToDo(evt.target.value);
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((current) => [toDo, ...current]);
+    setToDo("");
+  };
+  console.log('my arr', toDos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Dos ({ toDos.length })</h1>
+      <form onSubmit={ onSubmit }>
+        <input
+          type="text"
+          value={ toDo }
+          placeholder="Write your to do..."
+          onChange={ onChange }
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        { toDos.map((todo, index) => {
+          <li key={index}>{ todo }</li>
+        }) }
+      </ul>
     </div>
-  );
+  )
 }
-
 export default App;
