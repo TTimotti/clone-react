c913e3efdbf74be125508687cf32be8bb742bb7d
TTimotti <ttimotti@kakao.com>
2023-11-28 15:09:25 +0900

    change Route path

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Detail from './routes/Detail';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hello" element={<h1>Hello!</h1>} />
-        <Route path="/movie/:id" element={<Detail />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path={`${process.env.PUBLIC_URL}/hello`} element={<h1>Hello!</h1>} />
+        <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
       </Routes>
     </Router>
   )
